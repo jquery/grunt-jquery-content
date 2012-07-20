cac827a7fd62674c74934f8594b8ac5e54746547
adam j. sontag <ajpiano@ajpiano.com>
2012-07-20 15:29:32 -0400

    Also allow "data-linenum='true'" to trigger line numbers instead of causing "NaN" to print as line numbers

--- tasks/build.js
+++ tasks/build.js
@@ -125,8 +125,9 @@ grunt.registerHelper("syntax-highlight", function( options, callback ) {
 			var $t = $(this),
 			code = $t.html(),
 			lang = $t.attr("data-lang") || getLanguageFromClass( $t.attr("class") ) || crudeHTMLcheck( code ),
-			linenum = $t.attr("data-linenum") || 1,
-			gutter = $t.attr("data-linenum") === undefined ? false : true,
+			linenumAttr = $t.attr("data-linenum"),
+			linenum = (linenumAttr === "true" ? 1 : linenumAttr) || 1,
+			gutter = linenumAttr === undefined ? false : true,
 			brush = nsh.getLanguage( lang ) || nsh.getLanguage( "js" ),
 			highlighted = nsh.highlight( code, brush, {
 				"first-line": linenum,
