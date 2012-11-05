f08334edda3f027a165ecf383d60d623118c13a4
Jörn Zaefferer <joern.zaefferer@gmail.com>
2012-11-05 16:10:11 +0100

    Use strict

--- tasks/build-xml.js
+++ tasks/build-xml.js
@@ -1,4 +1,5 @@
 module.exports = function( grunt ) {
+"use strict";
 
 var fs = require( "fs" ),
 	path = require( "path" );
--- tasks/build.js
+++ tasks/build.js
@@ -1,4 +1,5 @@
 module.exports = function(grunt) {
+"use strict";
 
 function htmlEscape(text) {
 	return text
