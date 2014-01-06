e586cc90a0c1ebb932e1d9c34a14889d5e74f395
Scott González <scott.gonzalez@gmail.com>
2014-01-06 11:51:56 -0500

    Markdown: Generate permalinks for headers
    Ref gh-36

--- tasks/build.js
+++ tasks/build.js
@@ -239,35 +239,37 @@ grunt.registerHelper( "parse-markdown", function( src, generateToc ) {
 		tokens = marked.lexer( src ),
 		links = tokens.links;
 
-	if ( generateToc ) {
-		tokens.filter(function( item ) {
-			if ( item.type !== "heading" ) {
-				return false;
-			}
+	tokens.forEach(function( item ) {
+		if ( item.type !== "heading" ) {
+			return;
+		}
 
-			// Store original text and create an id for linking
-			item.tocText = item.text;
-			item.tocId = item.text
-				.replace( /\W+/g, "-" )
-				.replace( /^-+|-+$/, "" )
-				.toLowerCase();
+		// Store original text and create an id for linking
+		var parsedText = marked( item.text );
+		parsedText = parsedText.substring( 3, parsedText.length - 5 );
+		item.tocText = parsedText.replace( /<[^>]+>/g, "" );
+		item.tocId = item.tocText
+			.replace( /\W+/g, "-" )
+			.replace( /^-+|-+$/, "" )
+			.toLowerCase();
 
-			// Convert to HTML
-			item.type = "html";
-			item.pre = false;
+		// Convert to HTML
+		item.type = "html";
+		item.pre = false;
 
-			// Insert the link
-			item.text = "<h" + item.depth + " class='toc-linked'>" +
-				"<a href='#" + item.tocId + "' id='" + item.tocId + "' class='icon-link'>" +
-					"<span class='visuallyhidden'>link</span>" +
-				"</a> " + item.text + "</h" + item.depth + ">";
+		// Insert the link
+		item.text = "<h" + item.depth + " class='toc-linked'>" +
+			"<a href='#" + item.tocId + "' id='" + item.tocId + "' class='icon-link toc-link'>" +
+				"<span class='visuallyhidden'>link</span>" +
+			"</a> " + parsedText + "</h" + item.depth + ">";
 
-			return true;
-		}).forEach(function( item ) {
+		if ( generateToc ) {
 			toc += new Array( (item.depth - 1) * 2 + 1 ).join( " " ) + "* " +
 				"[" + item.tocText + "](#" + item.tocId + ")\n";
-		});
+		}
+	});
 
+	if ( generateToc ) {
 		tokens = marked.lexer( toc ).concat( tokens );
 		// The TOC never generates links, so we can just copy the links directly
 		// from the original tokens.
