51cd8c57fc6ccdf6408664ba2ed6376760a3d296
Scott González <scott.gonzalez@gmail.com>
2014-12-04 13:10:42 -0500

    Exports: Expose syntax highlighter to replace old helper

--- index.js
+++ index.js
@@ -1,3 +1,7 @@
+var syntaxHighlight = require( "./lib/highlight" );
+
+exports.syntaxHighlight = syntaxHighlight;
+
 exports.preprocessPost = function( post, fileName, callback ) {
 	callback( null, post );
 };
